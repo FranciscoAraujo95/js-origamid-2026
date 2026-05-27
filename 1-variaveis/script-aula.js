acd96537bb036eb8553cfeef9c777eb5d250ef63
Francisco Araujo <fhydglan@gmail.com>
2026-05-27 14:39:48 -0300

    Variáveis

---
+++ 1-variaveis/script-aula.js
@@ -0,0 +1,29 @@
+var nome = "Francisco";
+var idade = 28;
+var possuiFaculdade = false;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Araujo",
+  cidade = "Fortaleza";
+
+var semDefinir;
+console.log(semDefinir);
+
+// var aindaNaoDefini;
+
+var comida;
+console.log(comida);
+comida = "Pizza";
+console.log(comida);
+
+const time = "Vasco";
+time = "Flamengo";
+
+console.log(time);
---
+++ 1-variaveis/script.js
@@ -0,0 +1,16 @@
+// Exercício
+// Declarar uma variável com o seu nome
+let nome = "Francisco";
+
+// Declarar uma variável com a sua idade
+let idade = 30;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+
+// Declarar 5 variáveis diferentes sem valores
+let sexo, cidade, carro, moto, anoDeNascimento;
