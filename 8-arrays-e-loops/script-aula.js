b9f0c033a13fed13001add357d234f81c1fd3a29
Francisco Araujo <fhydglan@gmail.com>
2026-05-28 17:01:25 -0300

    Arrays & Loops

---
+++ 8-arrays-e-loops/script-aula.js
@@ -0,0 +1,29 @@
+// console.log(videoGames);
+// console.log(videoGames[0]);
+// console.log(videoGames[2]);
+
+// const ultimoItem = videoGames.pop();
+// console.log(ultimoItem);
+// console.log(videoGames);
+// console.log(videoGames.length);
+
+// For Loop
+var videoGames = ["Switch", "Ps5", "Xbox", "3DS"];
+// for (let i = 0; i < videoGames.length; i++) {
+//   console.log(`${i} ${videoGames[i]}`);
+//   if (videoGames[i] === "Ps5") {
+//     break;
+//   }
+// }
+
+var frutas = ["Banana", "Pera", "Maçã", "Abacaxai", "Uva"];
+frutas.forEach(function (item, index) {
+  console.log(index, item);
+});
+
+// While Loop
+// var i = 0;
+// while (i <= 100) {
+//   console.log(i);
+//   i += 5;
+// }
