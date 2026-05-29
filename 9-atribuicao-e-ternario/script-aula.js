393bf956bb4fa8cc8db0413537da9b0392173db1
Francisco Araujo <fhydglan@gmail.com>
2026-05-29 09:12:12 -0300

    Atribuição e Ternário

---
+++ 9-atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,17 @@
+// Operadores de atribuição
+var x = 5;
+var y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+var numero = 20;
+numero += 10;
+console.log(numero);
+
+let idade = 19;
+let podeBeber = idade >= 18 ? "Pode beber" : "Não pode beber";
+console.log(podeBeber);
