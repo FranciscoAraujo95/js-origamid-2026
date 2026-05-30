ef64296ef3bddd14a7eb892a6f71d8af209a3cb8
Francisco Araujo <fhydglan@gmail.com>
2026-05-30 14:57:02 -0300

    forEach & Arrow Function

---
+++ DOM/11-DOM/4-foreach-e-arrow-function/js/script-aula.js
@@ -0,0 +1,23 @@
+// const imgs = document.querySelectorAll("img");
+
+// imgs.forEach(function (item, index, array) {
+//   // console.log(item, index, array);
+// });
+
+const titulos = document.getElementsByClassName("titulo");
+const arrayTitulos = Array.from(titulos);
+
+// console.log(titulos);
+// console.log(arrayTitulos);
+
+arrayTitulos.forEach(function (item) {
+  // console.log(item);
+});
+
+// Arrow function
+
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((item) => {
+  console.log(item);
+});
