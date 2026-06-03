eaa87c1b11dd9382b7075192e06e79ce4b35ebf7
Francisco Araujo <fhydglan@gmail.com>
2026-06-03 14:53:05 -0300

    Native, Host & User 2

---
+++ objetos/3-native-host-e-user/script-aula.js
@@ -0,0 +1,7 @@
+console.log(typeof Array.fromdasd !== "undefined");
+
+if (typeof Array.fromdasd !== "undefined") {
+  console.log("Existe");
+} else {
+  console.log("Não existe");
+}
--- objetos/3-native-host-e-user/script.js
+++ objetos/3-native-host-e-user/script.js
@@ -1,6 +1,21 @@
-console.log(typeof Array.fromdasd !== "undefined");
+// Liste 5 objetos nativos
+Object;
+Number;
+String;
+Boolean;
+Function;
 
-if (typeof Array.fromdasd !== "undefined") {
+// Liste 5 objetos do browser
+Window;
+History;
+Document;
+HTMLCollection;
+NodeList;
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+if (typeof document.webkitVisiblilityState !== "undefined") {
   console.log("Existe");
 } else {
   console.log("Não existe");
